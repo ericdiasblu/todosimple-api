697adbd739c4c16140cc3c99108ef9f01ee85cf4
Eric Dias <ericdiasblu@gmail.com>
2025-05-06 20:43:53 -0300

    Create Task Repository

---
+++ src/main/java/com/dias/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.dias.todosimple.repositories;
+
+import com.dias.todosimple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(long id);
+
+    //    @Query(value = "SELECT t FROM Task t WHERE t.user.id = : id")
+    //    List<Task> findByUser_Id(@Param("id") long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = : id",nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") long id);
+
+
+}
