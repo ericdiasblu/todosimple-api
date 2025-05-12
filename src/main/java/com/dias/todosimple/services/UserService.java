d0f46fa926b3a3592498d9130c98aaf92ef5e967
Eric Dias <ericdiasblu@gmail.com>
2025-05-12 15:10:22 -0300

    Create TaskService

--- src/main/java/com/dias/todosimple/services/UserService.java
+++ src/main/java/com/dias/todosimple/services/UserService.java
@@ -1,7 +1,6 @@
 package com.dias.todosimple.services;
 
 import com.dias.todosimple.models.User;
-import com.dias.todosimple.repositories.TaskRepository;
 import com.dias.todosimple.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,9 +15,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
@@ -31,7 +27,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
