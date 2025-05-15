26156f9ecee78886c09087b7aaafc6ac09c9182d
Eric Dias <ericdiasblu@gmail.com>
2025-05-15 14:49:34 -0300

    Add Lombok

--- src/main/java/com/dias/todosimple/models/Task.java
+++ src/main/java/com/dias/todosimple/models/Task.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Objects;
 
@@ -15,8 +13,9 @@ import java.util.Objects;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Task {
-    public Task(){}
     public static final String TABLE_NAME = "task";
 
     @Id
@@ -33,29 +32,4 @@ public class Task {
     @NotEmpty
     @Size(min = 1, max = 255)
     private String description;
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof Task))
-            return false;
-        Task other = (Task) obj;
-        if (this.id == null)
-            if (other.id != null)
-                return false;
-            else if (!this.id.equals(other.id))
-                return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) && Objects.equals(this.description, other.description);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
 }
